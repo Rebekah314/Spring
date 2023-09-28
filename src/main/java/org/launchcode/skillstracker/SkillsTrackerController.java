1817401ed2cca9c2d9ea432d485309eb25a07ae3
Rebekah Garris <rebekahlgarris@gmail.com>
2023-09-27 19:17:27 -0500

    setting up application

---
+++ src/main/java/org/launchcode/skillstracker/SkillsTrackerController.java
@@ -0,0 +1,26 @@
+package org.launchcode.skillstracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class SkillsTrackerController {
+    @GetMapping
+    public String landingPage() {
+        return "<html>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Python</li>" +
+                "</ol>" +
+                "<body>" +
+                "<html>";
+    }
+
+
+}
